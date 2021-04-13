32bc24c86bd23fa682d650d8d2946bf81a9a38cc
jordytheboy <70452593+jordytheboy@users.noreply.github.com>
2021-04-13 11:56:33 -0500

    Added comments

--- chess/fishstock/interface.py
+++ chess/fishstock/interface.py
@@ -5,16 +5,20 @@ import pygame
 from pygame.constants import *
 import sys
 
+# Initialize pygame display
 width, height = 800, 800
 screen = pygame.display.set_mode((width, height))
 
+# Load in images
 surface = pygame.image.load('resources\\Board2.png').convert()
 surface = pygame.transform.scale(surface, (width, height))
 
+# Render images
 rect = surface.get_rect()
 rect = rect.move((0, 0))
 screen.blit(surface, rect)
 
+# Main loop
 run = True
 while run:
     for event in pygame.event.get():
